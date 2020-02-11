2d111fa4ff8ab9a901483ecb1598a02a69444f29
gradiuscypher <gradiuscypher@gmail.com>
2020-02-11 00:07:19 -0800

    kinda working

--- alpha.system.tasker.js
+++ alpha.system.tasker.js
@@ -1,9 +1,14 @@
 // manages what tasks each of the screeps should be working on
+var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder')
 
 // TODO IDEA:
 // Separate tasks into criticality levels: critical, high, medium, low, trivial
 // Determine which tasks can be completed and which ones can wait until the next available creep
 // Determine the number of creeps required per task, per level of criticality, and deploy based on those needs
+
+// TODO:
 // Spawn more creeps based on minimum number of creeps or need
 // Include ability to raise criticality of tasks if they've been ignored too long, maybe measure need on a scale from 0 to 100
 
@@ -31,16 +36,22 @@ var alphaSystemTasker = {
         // does the spawner need more energy
         // TODO: Finish
         var spawnerEnergyPercent = (Game.spawns['Spawn1'].energy / Game.spawns['Spawn1'].energyCapacity) * 100;
-        console.log(spawnerEnergyPercent);
+
+        // test values
+        // var spawnerEnergyPercent = 50;
+        // var availableWorkerCount = 6;
+
         if(spawnerEnergyPercent <= 25) {
-            // put half the creeps on spawner
-            Memory.tasklist = Memory.tasklist.concat(Array.apply(null, Array(availableWorkerCount/2)).map(_ => 'spawnerEnergy'));
+            var spawnCount = Math.floor(availableWorkerCount * .80);
+            Memory.tasklist = Memory.tasklist.concat(Array.apply(null, Array(spawnCount)).map(_ => 'spawnerEnergy'));
         }
         else if(spawnerEnergyPercent <= 50) {
-            Memory.tasklist = Memory.tasklist.concat(Array.apply(null, Array(2)).map(_ => 'spawnerEnergy'));
+            var spawnCount = Math.floor(availableWorkerCount * .50);
+            Memory.tasklist = Memory.tasklist.concat(Array.apply(null, Array(spawnCount)).map(_ => 'spawnerEnergy'));
         }
         else if(spawnerEnergyPercent <= 75) {
-            Memory.tasklist = Memory.tasklist.concat(Array.apply(null, Array(2)).map(_ => 'spawnerEnergy'));
+            var spawnCount = Math.floor(availableWorkerCount * .25);
+            Memory.tasklist = Memory.tasklist.concat(Array.apply(null, Array(spawnCount)).map(_ => 'spawnerEnergy'));
         }
 
         // keep the resources up (TODO)
@@ -50,31 +61,40 @@ var alphaSystemTasker = {
         // do any structures (link, storage, towers, walls, roads) need repairs (less than a threshold)
 
         // housekeeping tasks: split the remaining count of creeps on these tasks
-        // upgrade the room controller
-        // pickup dropped energy
+        var remainingCreepCount = availableWorkerCount - Memory.tasklist.length;
+        if(remainingCreepCount > 0) {
+            // upgrade the room controller
+            Memory.tasklist = Memory.tasklist.concat(Array.apply(null, Array(Math.ceil(remainingCreepCount/2))).map(_ => 'controllerUpgrade'));
+            // pickup dropped energy
+            Memory.tasklist = Memory.tasklist.concat(Array.apply(null, Array(Math.ceil(remainingCreepCount/2))).map(_ => 'collectEnergy'));
+        }
 
-        for(var creep in Game.creeps) {
+        if(Game.time % 2 == 0) {
+            console.log(Memory.tasklist);
+        }
+
+        for(var name in Game.creeps) {
+            var creep = Game.creeps[name];
             // if the creep is a specialist, do your special job
             // heavy miner
             // transporter
 
             // otherwise, pick up a task off the list
-            // console.log(Memory.tasklist.pop());
+            var targetTask = Memory.tasklist.shift();
+
+            switch (targetTask) {
+                case 'spawnerEnergy':
+                    // roleHarvester.run(creep);
+                    break;
+                case 'controllerUpgrade':
+                    // roleUpgrader.run(creep);
+                    break;
+                case 'collectEnergy':
+                    // roleUpgrader.run(creep);
+                    break;
+            }
+
         }
-        console.log(Memory.tasklist);
     }
 }
 module.exports = alphaSystemTasker;
-
-
-function mineEnergy(creep) {
-    console.log("I would have mined.");
-}
-
-function refillController(creep) {
-    console.log("Refilling the controller.");
-}
-
-function refillSpawner(creep) {
-    console.log("Refilling the Spawn.");
-}
